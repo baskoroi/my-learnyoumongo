0883f8e935ec626cb3d5bf811a5d6d27643f68de
Baskoro Indrayana <baskoroindrayana@outlook.com>
2016-09-09 11:49:49 +0700

    Solved Exercise 8

---
+++ count.js
@@ -0,0 +1,18 @@
+const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017/learnyoumongo';
+const age = parseInt(process.argv[2]);
+
+mongo.connect(url, (err, db) => {
+    if (err) throw err;
+
+    db.collection('parrots').count({
+        age: {
+            $gt: age
+        }
+    }, (err, count) => {
+        if (err) throw err;
+        console.log(count);
+    });
+
+    db.close();
+});
