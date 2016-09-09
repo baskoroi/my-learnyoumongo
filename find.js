92ba8bf17655505aa77da610db70800a4d5abbd3
Baskoro Indrayana <baskoroindrayana@outlook.com>
2016-09-09 10:18:41 +0700

    Modified solution: Exercise 3

--- find.js
+++ find.js
@@ -2,10 +2,7 @@ const mongo = require('mongodb').MongoClient;
 const url = 'mongodb://localhost:27017/learnyoumongo';
 
 mongo.connect(url, (err, db) => {
-    if (err) {
-        console.error('Error connecting to db:', err);
-        throw err;
-    }
+    if (err) throw err;
 
     let parrots = db.collection('parrots');
     const age = parseInt(process.argv[2]);
@@ -14,11 +11,7 @@ mongo.connect(url, (err, db) => {
             $gt: age
         }
     }).toArray((err, docs) => {
-        if (err) {
-            console.error('Error finding docs:', err);
-            throw err;
-        }
-
+        if (err) throw err;
         console.log(docs);
     });
     
